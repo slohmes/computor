7dbe9be624fe197d9958d1abaeab47ef18acd509
Sarah Lohmeier <sarahlohmes@gmail.com>
2018-05-11 14:09:38 -0400

    Corrects AND operation behavior, includes minor code clean-ups

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
 const booleans = {
-  0: false,
-  1: true
+  '0': false,
+  '1': true
 };
 
 const logicGates = {
-  '00': (a, b) => ('' + a + b), // return input unchanged
-  '01': (a, b) => (a && b), // AND
-  '10': (a, b) => (a || b), // OR
-  '11': (a, b) => ((booleans[a] ? '0' : '1') + (booleans[b] ? '0' : '1')) // NOT
+  '00': (a, b) => false, // do nothing
+  '01': (a, b) => a && b, // AND
+  '10': (a, b) => a || b, // OR
+  '11': (a, b) => !a // NOT
 };
 
 document.getElementById('compute').onclick = () => {
@@ -16,13 +16,15 @@ document.getElementById('compute').onclick = () => {
   let input = [];
   document.getElementById('punchcard').childNodes.forEach((child) => {
     if (child.type === 'checkbox') {
-      input += child.checked ? 1 : 0;
+      input += child.checked ? '1' : '0';
     }
   });
-  const opCode = '' + input[0] + input[1];
-  const output = logicGates[opCode](input[2], input[3]);
+  const opCode = input[0] + input[1];
+  console.log('opCode: ', opCode);
+  console.log('input: ', input[2], input[3]);
+  const output = logicGates[opCode](booleans[input[2]], booleans[input[3]]);
+  console.log('output: ', output);
 
   // display output
-  document.getElementById('output-0').checked = booleans[output[0]];
-  document.getElementById('output-1').checked = booleans[output[1]];
+  document.getElementById('output-0').checked = output;
 };
